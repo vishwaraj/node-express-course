bebe957e529c59ce58a03c9fe80ceffa8ff40060
vraj <vraj@vishwarajbhattrai.local>
2020-11-15 01:05:59 +0530

    add login post route

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express=require('express');
 const app=express();
 
+const bodyparser=require('body-parser');
+app.use(bodyparser.json());
+
 const mockUserData=[
 {name:'Mark'},
 {name:'Jill'}
@@ -24,6 +27,28 @@ app.get('/users/:id',function(req,res){
 	})
 })
 
+
+
+app.post('/login',function(req,res){
+ 	const username=req.body.username;
+ 	const password=req.body.password;
+ 
+ 	const mockUsername="billyTheKid";
+ 	const mockPassword="superSecret";
+ 
+ 	if (username===mockUsername && password===mockPassword){
+      	res.json({
+      	 	success: true,
+      	 	message: 'password and username match!',
+      	 	token: 'encrypted token goes here'
+      	})
+ 	} else {
+      	res.json({
+      	 	success: false,
+      	 	message: 'password and username do not match'
+      	})
+ 	}
+})
 
 app.listen(8000,function(){
 
